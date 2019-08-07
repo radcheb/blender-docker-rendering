5cacc1900e8166cdf2cd987bea65575605ed295e
Radhwane Chebaane <radcheb@gmail.com>
2019-08-07 21:42:23 +0200

    building and running image with gpu support

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
+# CPU verison
 docker build -t docker-blender:2.8-cpu -f Dockerfile.cpu .
 docker run --rm -v $(pwd)/media/:/media/ docker-blender:2.8-cpu
+
+if [ -x nvidia-smi ]; then
+	# GPU verison
+	docker build -t docker-blender:2.8-gpu -f Dockerfile.gpu .
+	docker run --rm --runtime nvidia -v $(pwd)/media/:/media/ docker-blender:2.8-gpu
+fi
